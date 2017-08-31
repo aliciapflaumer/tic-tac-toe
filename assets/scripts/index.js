1c974096754f7ca057f9ae4a805f42b9febc5fd7
aliciapflaumer <alpfla7@gmail.com>
2017-08-31 14:24:39 -0400

    Game logic
    When I click a cell I am able to identify what cell was clicked in the
    console.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,12 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-const events = require('events')
+const events = require('./events')
 
 $(() => {
   setAPIOrigin(location, config)
+
+
 })
 
 // use require with a reference to bundle the file and use it in this file
