927af7d492061ecfb74479c84993620614e4ac6b
aliciapflaumer <alpfla7@gmail.com>
2017-09-01 16:34:55 -0400

    Added game scripts
    Figuring out game logic

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -50,60 +50,6 @@ const addHandlers = function () {
   $('#sign-out').on('submit', onSignOut)
 }
 
-
-// Start with empty board of 9 cells
-// const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-// check whose turn it is
-
-// if it's x's turn place value into cell
-
-// if it's not x's turn place the value o into cell
-
-// const turn = 'x'
-
-const onClick = function () {
-// if you click any cell fill an x
-// on the click of any divs with ids that begin
-// with cell want to change text
-
-  //  if (turn === turn) {
-  //   // if it's x's turn put value x in cell
-  //   $("div[id^='cell']").on('click', function (event) {
-  //     // target element that we click on and change text
-  //      $(this).text('x')
-  //      console.log('It\'s x\'s turn')
-  //   })
-  //
-  // } else {
-  //   // if it's o's turn put value o in cell
-  //   $("div[id^='cell']").on('click', function (event) {
-  //     // target element that we click on and change text
-  //      $(this).text('o')
-  //      console.log('It\'s o\'s turn')
-  //   })
-  //
-  // }
-
-    $("div[id^='cell']").on('click', function (event) {
-    console.log(this)
-    // target element that we click on and change text
-     $(this).text('X')
-  })
-}
-
-onClick()
-
-// if it's o's turn fill cell with an o,
-// else stop x from filling a square and fill square with o
-
-// if one player gets 3 in a row, show player wins in ui.js
-// and end play so no one can play anymore squares
-
-
-
-
-
 module.exports = {
   addHandlers
 }
---
+++ curl/game-scripts/update-game.sh
@@ -0,0 +1,6 @@
+curl "http://tic-tac-toe.wdibos.com/" \
+--request PATCH  \
+--header "Authorization: Token token=<token>" \
+--data '{
+  
+}'
