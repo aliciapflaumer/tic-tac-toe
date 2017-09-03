49c836fe510dd9ba1600fe1368883950b9540c12
aliciapflaumer <alpfla7@gmail.com>
2017-09-03 18:46:29 -0400

    Game logic in events.js
    I didn't get anything done.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -8,30 +8,30 @@
 // if one player gets 3 in a row, show player wins in ui.js
 // and end play so no one can play anymore squares
 
+const turn = "X"
+const moves = 0
 
 // Start with empty board of 9 cells
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+]
 
-const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-//
-// const createGame = function () {
-//
-// }
+const onClickMove = function () {
+// if player 1 clicks cell, fill cell with an x
+$("div[id^='cell']").on('click', function (event) {
+  event.preventDefault()
+// console.log(this)
+// target element that we click on and change text
+ $(this).text(turn)
+})
 
-const turn = function () {
-// // if you click any cell fill an x
-// // on the click of any divs with ids that begin
-// // with cell want to change text
-    $("div[id^='cell']").on('click', function (event) {
-      event.preventDefault()
-    console.log(this)
-    // target element that we click on and change text
-     $(this).text('X')
-  })
 }
-turn()
+onClickMove()
+
 
 
 module.exports = {
-  // board,
-  // createGame
+
 }
