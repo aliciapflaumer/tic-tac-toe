a2c5434fcf4aa3396edf373a1432830583fdcc77
aliciapflaumer <alpfla7@gmail.com>
2017-09-05 11:47:05 -0400

    Fixing merge conflict
    Committing files worked on first.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -7,55 +7,11 @@ const ui = require('./ui')
 
 // game events
 
-// check whose turn it is
-// if it's x's turn place value into cell
-// if it's not x's turn place the value o into cell
-// if it's o's turn fill cell with an o,
-// else stop x from filling a square and fill square with o
-// if one player gets 3 in a row, show player wins in ui.js
-// and end play so no one can play anymore squares
-
-
-// const onClickMove = function () {
-// // if player 1 clicks cell, fill cell with an x
-// $("div[id^='cell']").on('click', function (event) {
-//   event.preventDefault()
-// console.log(this)
-// // target element that we click on and change text
-//  // $(this).text("X")
-// })
-// }
-// onClickMove()
-
-
-// const postCreateGameSuccess = function () {
-//   store.game = data.game
-// }
-//
-// // Event handlers for when users click on game cells
-// const onClickCellZero = function (event) {
-//   const data = getFormFields(this)
-//
-//     if (store.game.cells[0] === '') {
-//       store.game.cells[0] = 'X'
-//       console.log(store.game.cells[0])
-//     } else {
-//       if (store.game.cells[0] !== '') {
-//         console.log('this cell is full!')
-//       }
-//     }
-//   event.preventDefault()
-//   api.onClickCellZero()
-//     .then(ui.onClickCellZero)
-//     .catch(ui.onClickCellZero)
-// }
-// const addHandlers = function () {
-//   $('#cell0').on('click', onClickCellZero)
-// }
-
 let turn = 'X'
 
-$("div[id^='cell']").on('click', function (event) {
+// setting up event listener
+// "div[id^='cell']"
+$('.box').on('click', function (event) {
   event.preventDefault()
 
   console.log(this)
@@ -63,6 +19,7 @@ $("div[id^='cell']").on('click', function (event) {
 
  if ($(this).val() === '') {
    // target element that we click on and change text
+
     if (turn === 'X') {
       $(this).text('X')
       turn = 'O'
@@ -72,4 +29,9 @@ $("div[id^='cell']").on('click', function (event) {
     }
  }
 
+
 })
+
+if ('.box' > 'X' || 'O').html() !== '') {
+  $
+}
