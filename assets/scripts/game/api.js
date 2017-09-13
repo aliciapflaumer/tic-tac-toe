5cffaf4bb9f0ac5d3635f506539dd10ca073e813
aliciapflaumer <alpfla7@gmail.com>
2017-09-13 18:21:36 -0400

    Created createGameSuccess handlers
    Got the functionality to work when a user clicks new game
    button and displays message that it's X's turn.

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -5,15 +5,19 @@ const store = require('../store')
 
 const createGame = function (data) {
   return $.ajax({
-    url: config.apiOrigin + '/create',
+    url: config.apiOrigin + '/games',
     method: 'POST',
+    // add Token
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
     data
   })
 }
 
 const updateGame = function (data) {
   return $.ajax({
-    url: config.apiOrigin + '/games/' + store.game.id,
+    url: config.apiOrigin + '/games/:id' + store.game.id,
     method: 'PATCH',
     contentType: 'application/json',
     // add Token
--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,15 +2,33 @@
 
 const store = require('../store')
 
-// const createGameSuccess = (data) {
-//   $('#game-message').text("It\'s X\'s turn!")
-// }
-//
-// const createGameFail = function(data) {
-//   $('#message').text("There seems to be an error creating game!")
-// }
-//
-// module.exports = {
-//   createGameSuccess,
-//   createGameFail
-// }
+const createGameSuccess = function (data) {
+  // console.log(data)
+  console.log('Successfully created game!')
+  $('#game-message').text("It\'s X\'s turn!")
+  $('#message').hide()
+}
+
+const createGameFail = function (data) {
+  // console.log(data)
+  $('#message').text("There seems to be an error creating game!")
+}
+
+const updateGameSuccess = function (data) {
+  console.log(data)
+
+
+}
+
+const updateGameFail = function (data) {
+  console.log(data)
+
+  
+}
+
+module.exports = {
+  createGameSuccess,
+  createGameFail,
+  updateGameSuccess,
+  updateGameFail
+}
