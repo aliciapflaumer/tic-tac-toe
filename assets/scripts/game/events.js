c02365991f603ab24c0a7867a1821f27cbc347bb
aliciapflaumer <alpfla7@gmail.com>
2017-09-17 22:15:44 -0400

    made some final fixes

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -8,6 +8,25 @@ const api = require('./api')
 const ui = require('./ui')
 
 
+// Update the data every time a user makes a move
+const onUpdateGame = function (index, value, over) {
+  console.log('onUpdateGame event function reached')
+
+  const data = {
+    'game': {
+      'cell': {
+        'index': index,
+        'value': value
+      },
+      'over': over
+    }
+  }
+  // console.log(data)
+
+  api.updateGame(data)
+  .then(ui.updateGameSuccess)
+  .catch(ui.updateGameFailure)
+}
 
 // game events
 const addHandlers = function () {
@@ -33,7 +52,7 @@ const addHandlers = function () {
   api.getGames()
     .then(ui.getGamesSuccess)
   })
-  
+
 let turn = 'X'
 
 // setting up event listener
@@ -54,14 +73,11 @@ $('.box').on('click', function (event) {
         }
       }
 
-
-
-
   // If tie game
     if ($('#cell0').text() !== '' && $('#cell1').text() !== '' && $('#cell2').text() !== '' &&
     $('#cell3').text() !== '' && $('#cell4').text() !== '' && $('#cell5').text() !== '' &&
     $('#cell6').text() !== '' && $('#cell7').text() !== '' && $('#cell8').text() !== '') {
-      $('#game-message').text('Game Over. It\'s a draw. No one wins this game.').fadeIn(5000),
+      $('#game-message').text('Game Over. It\'s a draw. No one wins this game.').fadeIn(5000).fadeOut(5000),
       $('.game-board').hide()
     }
 
@@ -153,25 +169,8 @@ if ($('#cell0').text() === 'O' && $('#cell1').text() === 'O' && $('#cell2').text
 
 } // end addHandlers function
 
-// Update the data every time a user makes a move
-const onUpdateGame = function (index, value, over) {
-  // console.log('onUpdateGame event function reached')
 
-  const data = {
-    'game': {
-      'cell': {
-        'index': index,
-        'value': value
-      },
-      'over': over
-    }
-  }
-  // console.log(data)
 
-  api.updateGame(data)
-  .then(ui.updateGameSuccess)
-  .catch(ui.updateGameFailure)
-}
 
 module.exports = {
   addHandlers,
